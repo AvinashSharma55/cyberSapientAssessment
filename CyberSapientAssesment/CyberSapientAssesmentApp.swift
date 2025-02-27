8843f6fca1172862e3c214765c7f5685be58a7cd
Avinash Sharma <avinashsharma8334@yahoo.in>
2025-02-27 20:26:43 +0530

    Initial Commit

---
+++ CyberSapientAssesment/CyberSapientAssesmentApp.swift
@@ -0,0 +1,20 @@
+//
+//  CyberSapientAssesmentApp.swift
+//  CyberSapientAssesment
+//
+//  Created by Avinash Sharma on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CyberSapientAssesmentApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
